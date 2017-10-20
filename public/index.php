9945913b9788c78b94ed8034023c372c97aa07c8
Marek Mazur <mazur@red-sky.pl>
2017-10-20 09:51:48 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+//phpinfo();
+
+$servername = "mysql";
+$username = "root";
+$password = "test";
+$dbname = 'mysql';
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
